b0ba3bbe29a4fa5745149eeb6be9924529c5a053
giammacarioca <giammattey@gmail.com>
2022-02-21 12:11:13 -0300

    route guarding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import { useAuthContext } from './hooks/useAuthContext'
 
 import Home from './pages/home/Home'
@@ -7,7 +7,7 @@ import Login from './pages/login/Login'
 import Navbar from './components/Navbar'
 
 function App() {
-  const { authIsReady } = useAuthContext()
+  const { authIsReady, user } = useAuthContext()
 
   return (
     <div className='App'>
@@ -16,13 +16,16 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path='/'>
-              <Home />
-            </Route>
-            <Route path='/signup'>
-              <Signup />
+              {!user && <Redirect to='/login' />}
+              {user && <Home />}
             </Route>
             <Route path='/login'>
-              <Login />
+              {user && <Redirect to='/' />}
+              {!user && <Login />}
+            </Route>
+            <Route path='/signup'>
+              {user && user.displayName && <Redirect to='/' />}
+              {!user && <Signup />}
             </Route>
           </Switch>
         </BrowserRouter>
